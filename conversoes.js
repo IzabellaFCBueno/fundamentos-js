3e6685f314db737040feabc604187b0f25ef86df
Izabella Bueno <izabellafcbueno@gmail.com>
2022-10-21 19:12:24 +0100

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleanos
+
+// conversão implicita x conversão explicita e NaN
+const numero = 456;
+const numeroString = Number("456a");
+
+// Number()
+// String()
+console.log(numero + numeroString);
+
+
